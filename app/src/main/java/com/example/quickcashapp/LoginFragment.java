829bebd6bdf8046f998d42774abde38ac20c30ea
lucasfisher <lc420263@dal.ca>
2024-09-25 12:21:48 -0300

    Working Navigation for post,search and login using fragments.

---
+++ app/src/main/java/com/example/quickcashapp/LoginFragment.java
@@ -0,0 +1,102 @@
+package com.example.quickcashapp;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LoginFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LoginFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LoginFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LoginFragment newInstance(String param1, String param2) {
+        LoginFragment fragment = new LoginFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    public interface loadPageListener{
+        public void loadPage(Fragment frag);
+    }
+    public loadPageListener loadListener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            loadListener = (loadPageListener) context;
+        } catch (ClassCastException castException) {
+            /** The activity does not implement the listener. */
+            throw new ClassCastException(context.toString() + " must implement OnDataSendListener");
+        }
+
+    }
+
+    @Override
+    public void onViewCreated(View view,Bundle savedInstanceState){
+        TextView registerNow = (TextView) getView().findViewById(R.id.registerNow);
+        registerNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadListener.loadPage(new RegistrationFragment());
+            }
+        });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+
+    }
+}
--- app/src/main/java/com/example/quickcashapp/MainActivity.java
+++ app/src/main/java/com/example/quickcashapp/MainActivity.java
@@ -3,41 +3,60 @@ package com.example.quickcashapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.quickcashapp.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity implements LoginFragment.loadPageListener {
+
+    @Override
+    public void loadPage(Fragment frag){
+        replaceFrag(frag);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        TextView registerButton = findViewById(R.id.register);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFrag(new LoginFragment());
 
-        // Set up the button click listener
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
-                startActivity(intent);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if(item.getItemId() == R.id.postJob) {
+                replaceFrag(new PostJobFragment());
+            }else if(item.getItemId() == R.id.login) {
+                replaceFrag(new LoginFragment());
+            }else if(item.getItemId() ==R.id.searchJobs){
+                    replaceFrag(new SearchJobFragment());
+            }else{
+                replaceFrag(new SearchJobFragment());
             }
+            return true;
         });
+
+
+
+
     }
 
+
+    public void replaceFrag(Fragment frag){
+        FragmentManager fragManager = getSupportFragmentManager();
+        FragmentTransaction fragTransaction = fragManager.beginTransaction();
+        fragTransaction.replace(R.id.frameLayout,frag);
+        fragTransaction.commit();
+    }
 
 
 }
