01c6753bb84800372eb6f628f1fc32c40a185e62
Yeobin Lee <yb921775@dal.ca>
2024-09-26 13:31:39 -0300

    Choosing role push

--- app/src/main/java/com/example/quickcashapp/MainActivity.java
+++ app/src/main/java/com/example/quickcashapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.quickcashapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btn1 = findViewById(R.id.button); //employer
+        Button btn2 = findViewById(R.id.button2); //employee
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(MainActivity.this,MainActivityEmployer.class);
+                startActivity(intent1);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(MainActivity.this,MainActivityEmployee.class);
+                startActivity(intent2);
+            }
         });
     }
 }
