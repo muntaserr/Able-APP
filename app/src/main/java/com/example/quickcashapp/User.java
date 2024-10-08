8fdda9c66d219f7eb299c6246bdee2409dc51561
lucasfisher <lc420263@dal.ca>
2024-10-07 23:47:24 -0300

    merged Login

---
+++ app/src/main/java/com/example/quickcashapp/User.java
@@ -0,0 +1,22 @@
+package com.example.quickcashapp;
+
+public class User {
+    public String name;
+    public String email;
+    public String password;
+    public String creditCard;
+    public String role;
+
+    public User() {
+
+    }
+
+    public User(String name, String email, String password, String creditCard, String role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.creditCard = creditCard;
+        this.role = role;
+    }
+}
+
