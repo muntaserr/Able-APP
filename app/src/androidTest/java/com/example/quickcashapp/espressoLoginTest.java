df05326c7bfbe97f81efb21cb95b47404957e696
Yeobin Lee <yb921775@dal.ca>
2024-10-09 17:10:04 -0300

    Revert "Reapply "pull development branch + added UIAutomator Tests for employer dashboard + Added annotations to the employer dashboard code""
    This reverts commit b97cc82b30167dbbcacb85aaea96d917058b0406.

--- app/src/androidTest/java/com/example/quickcashapp/espressoLoginTest.java
+++ app/src/androidTest/java/com/example/quickcashapp/espressoLoginTest.java
@@ -1,16 +1,26 @@
 package com.example.quickcashapp;
 
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 
-import androidx.test.core.app.ActivityScenario;
+import android.content.Intent;
 
-import org.junit.Test;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class espressoLoginTest {
     @Test
