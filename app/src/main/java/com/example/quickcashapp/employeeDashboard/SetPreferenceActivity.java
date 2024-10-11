1233add429667792ce02d0c0e359541c863b5071
Ruijie Li <rj569393@dal.ca>
2024-10-11 17:36:37 -0300

    employee submit

--- app/src/main/java/com/example/quickcashapp/employeeDashboard/SetPreferenceActivity.java
+++ app/src/main/java/com/example/quickcashapp/employeeDashboard/SetPreferenceActivity.java
@@ -10,6 +10,5 @@ public class SetPreferenceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_jobs);  // Link to the layout XML file
-        // You can add more logic here (e.g., setting up views, buttons, etc.)
     }
 }
