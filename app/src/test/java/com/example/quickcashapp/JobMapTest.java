4da0cc3a911bf5ace887217bd2cff0345671205c
lucasfisher <lc420263@dal.ca>
2024-11-01 21:30:38 -0300

    replaced the main activity

---
+++ app/src/test/java/com/example/quickcashapp/JobMapTest.java
@@ -0,0 +1,31 @@
+package com.example.quickcashapp;
+
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.*;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+
+/**
+ * Unit tests for the logout portion
+ */
+public class JobMapTest {
+
+
+    @Before
+    public void setup(){
+
+    }
+
+    @Test
+    public void test_jobs_retrieved(){
+
+    }
+
+}
