4725cc12610c852ef28d776b286ea7869c2de436
John Rovic Manansala <jh621945@dal.ca>
2024-11-04 15:33:50 -0400

    Revert "Dev changes"
    This reverts merge request !42

--- app/src/main/java/com/example/quickcashapp/employeeDashboard/SearchJobsActivity.java
+++ app/src/main/java/com/example/quickcashapp/employeeDashboard/SearchJobsActivity.java
@@ -11,7 +11,4 @@ public class SearchJobsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_jobs);  // Link to the layout XML file
     }
-    public boolean AddtoPreference(){
-        return true;
-    }
 }
